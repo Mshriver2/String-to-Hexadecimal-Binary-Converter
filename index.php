148372694cea4694ac21f8aa9c070e5ef61caeef
Max S <creatingalong@gmail.com>
2019-11-11 18:18:53 -0600

    added select box

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 
     <form method="post">
         <br><input type="text" minlength = "1" name="string_value" placeholder="Enter string">
+        <select name="b_or_h"><option value="binary">Binary</option><option value="hexadecimal">Hexadecimal</option></select>
         <input name="new_password_submit" type="submit" value="Submit">
     </form>
 
