0b16677afa25214770586a35454fd1227f8e3014
Max S <creatingalong@gmail.com>
2019-11-11 18:03:19 -0600

    basic html form

--- index.php
+++ index.php
@@ -2,3 +2,24 @@
 //©2019 Max Shriver and Spencer Dyvig
 
 ?>
+
+
+
+<html>
+<head>
+
+</head>
+
+<body>
+
+    <form method="post">
+        <br><input type="text" minlength = "1" name="string_value" placeholder="Enter string">
+        <input name="new_password_submit" type="submit" value="Submit">
+    </form>
+
+
+
+</body>
+
+
+</html>
