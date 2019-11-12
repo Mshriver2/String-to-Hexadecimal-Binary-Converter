978ba8ae616177102f7d977d22eb9fa224f92576
Spencer Dyvig <sd31117@hotmail.com>
2019-11-11 18:11:12 -0600

    starting functions for converting string to binary

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
 //©2019 Max Shriver and Spencer Dyvig
+$str = ($_POST['string_value'])
 
+function strToBinary {
+  $stringCount = strlen($str)
+
+  for ($i = 0, $i < $stringCount, $i++) {
+
+  }
+}
+
+if(isset($_POST['submit'])) {
+  echo strToBinary($str)
+}
 ?>
