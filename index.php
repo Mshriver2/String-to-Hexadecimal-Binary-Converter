ad76818a9ddbf1eb9464dc37983f34cfa7c6b373
Spencer Dyvig <sd31117@hotmail.com>
2019-11-13 13:32:22 -0600

    fixed GPL notice again

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
-//Copyright (C) 2019 Max Shriver and Spencer Dyvig
-// This file is part of String to Hexadecimal Binary Converter
+/*
+This program converts text strings to binary and hexadecimal through a user interface.
+Copyright (C) 2019  Max Shriver, Spencer Dyvig
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 function strToBinary {
   $str = ($_POST['string_value'])
